3038ed63a313a45f1d6bc3f58880f72c4f1f5ad3
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-31 20:20:42 +0900

    Add Runtime permission request.

--- app/src/main/java/com/example/android/photobyintent/PhotoIntentActivity.java
+++ app/src/main/java/com/example/android/photobyintent/PhotoIntentActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.photobyintent;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,6 +10,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -40,6 +43,8 @@ public class PhotoIntentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        verifyStoragePermissions(this);
 
         mImageView = (ImageView) findViewById(R.id.imageView1);
         mImageBitmap = null;
@@ -217,6 +222,28 @@ public class PhotoIntentActivity extends AppCompatActivity {
             btn.setText(
                     getText(R.string.cannot).toString() + " " + btn.getText());
             btn.setClickable(false);
+        }
+    }
+
+    // Storage Permissions variables
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
         }
     }
 }
