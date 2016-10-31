61c67192bd14911bdce260557c10e083a81c6d2e
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-31 18:26:03 +0900

    First commit

---
+++ app/src/main/java/com/example/android/photobyintent/PhotoIntentActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.photobyintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
